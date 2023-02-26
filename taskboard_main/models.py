463735ee08feda15f93b738b4ef90d27ebeccd9c
saloni-singh-26 <salonisinghss123@gmail.com>
2023-02-26 15:42:26 +0530

    update : models created prouct , cart item , order_item, order_detail models added

--- taskboard_main/models.py
+++ taskboard_main/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=255)
+    sku = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    telephone = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+
+
+class Cart_item(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField(max_length=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+
+class Order_detail(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user_id.first_name
+
+
+class Order_item(models.Model):
+    order_id = models.ForeignKey(Order_detail, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
