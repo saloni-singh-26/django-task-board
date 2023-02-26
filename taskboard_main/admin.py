951db1cbb901f6347128927be00ac9a2b985cbc2
saloni-singh-26 <salonisinghss123@gmail.com>
2023-02-26 15:36:01 +0530

    feat : register model name all model name registered here

--- taskboard_main/admin.py
+++ taskboard_main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from taskboard_main.models import Product, User, Cart_item, Order_detail, Order_item
+
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(User)
+admin.site.register(Cart_item)
+admin.site.register(Order_detail)
+admin.site.register(Order_item)
