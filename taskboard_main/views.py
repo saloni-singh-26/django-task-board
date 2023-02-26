01ad192ffbaaa861f117929f94f358b3333d238a
saloni-singh-26 <salonisinghss123@gmail.com>
2023-02-26 15:40:58 +0530

    update : crud operation created using function based view create, update , delete ,get method created using rest api

--- taskboard_main/views.py
+++ taskboard_main/views.py
@@ -1,8 +1,54 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.response import Response
+
+from taskboard_main.models import Product
+from rest_framework.decorators import api_view
+
+from taskboard_main.serializers import ProductSerializer
+
 
 # Create your views here.
+from rest_framework import status
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def product_create(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-def home(self):
-    return HttpResponse('hello world!!!!!!')
+@api_view(['PUT'])
+def product_update(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["DELETE"])
+def product_delete(request, pk):
+    prod = Product.objects.get(id=pk)
+    prod.delete()
+    return Response("deleted...")
+
+
+
+
+
+
+
+
