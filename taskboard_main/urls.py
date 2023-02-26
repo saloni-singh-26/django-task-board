f0d27fe0ecbf5e76a5bbdc49a5b4719369836c85
saloni-singh-26 <salonisinghss123@gmail.com>
2023-02-26 15:38:42 +0530

    update : crud urls urls added

--- taskboard_main/urls.py
+++ taskboard_main/urls.py
@@ -3,6 +3,10 @@ from . import  views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('task-main', views.product_list, name='task'),
+    path('detail/<str:pk>/', views.product_detail, name='task-detail'),
+    path('create/', views.product_create, name='task-create'),
+    path('update/', views.product_update,name="product-update"),
+    path('delete/<str:pk>', views.product_delete, name="product-delete")
 
 ]
